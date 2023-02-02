1fee2e1f952d4c16a39bde063793dd1d2491efb9
Omar Rojas Aguirre <omar.rojas@sofka.com.co>
2023-02-01 21:43:18 -0500

    Ajustes Front

--- WebApplication1/Controllers/AccountController.cs
+++ WebApplication1/Controllers/AccountController.cs
@@ -111,7 +111,7 @@ public class AccountController : Controller
 				textopp3 = "PPROTECC was conceived as a management tool for the prevention of work accidents at heights, to strengthen the application of organizational policies";
 				textopp4 = "and ensure timely compliance with current legislation and regulations.";
 				modulos = "Working at Height|Lifting of loads|Confined spaces|Electric risk|Fire extinguishers management";
-				subtitulos = "The Best Way To Manage A Risk Is To Know About It|Is Lifting Loads The Most Important Process?|COMING SOON|COMING SOON|COMING SOON";
+				subtitulos = "The Best Way To Manage A Risk Is To Know About It|Is Lifting Loads The Most Important Process?|Coming soon|Coming soon|Coming soon";
 				descripciones = "Work at height is work that is carried out in any place where, if the necessary precautions have not been taken, a person can fall from a height that can cause injury (a fall through a fragile roof, down an unprotected elevator shaft down a stairwell).|First, what is a lift? We can understand what a hoist is as a way of lifting or moving objects with the help of some devices, which is done in a safe, controlled and well calculated way.|A confined space or confined space is one that has reduced entry openings, unfavorable natural ventilation and is not designed to remain inside. Therefore, it can present an unbreathable atmosphere and harbor toxic or flammable gases, vapors or particles.|Electrical risk is considered when there is a possibility of contact of the human body with the electric current and that can be a danger to the integrity of people.|More specifically, a fire extinguisher could be defined as an autonomous device, designed as a cylinder, which can be moved by a single person and which, using a drive mechanism under gas pressure or mechanical pressure, launches an extinguishing agent towards the base. of the fire, in order to extinguish it.";
 				
 				piedepaginacards = "Last inspections|6 months ago";
@@ -119,7 +119,7 @@ public class AccountController : Controller
 			}
             else if (lang == "spain")
             {
-				piedepaginacards = "Ultimas inspecciones|Hace 6 meses";
+				piedepaginacards = "Últimas inspecciones|Hace 6 meses";
 				mess = "Proceso completado con éxito";
 				Welcome = "BIENVENIDO AL PORTAL PPROTECC 2.0";
 				invite = "¡TE INVITAMOS A HACER PARTE DE ESTA EXPERIENCIA!";
@@ -128,7 +128,7 @@ public class AccountController : Controller
 				textopp3 = "PPROTECC fue pensado como herramienta de gestión para la prevención de accidentes de trabajo en alturas, para fortalecer la aplicación de las políticas organizacionales  y";
 				textopp4 = "velar por el cumplimiento oportuno de la  legislación y normatividad vigente.";
 				modulos = "Trabajo en alturas|Izaje de Cargas|Espacios confinados|Riesgo eléctrico|Manejo Extintores";
-				subtitulos = "La Mejor Forma De Administrar Un Riesgo Es Conocerlo|¿Es El Izaje De Cargas El Proceso Más Importante?|PROXIMAMENTE|PROXIMAMENTE|PROXIMAMENTE";
+				subtitulos = "La mejor forma de administrar un riesgo es conocerlo|¿Es el izaje de cargas el proceso más importante?|Próximamente|Próximamente|Próximamente";
 				descripciones = "Trabajo en altura es aquel que se realiza en cualquier lugar donde, si no se han adoptado las precauciones necesarias, una persona puede caer desde una altura que puede provocar lesiones (una caída a través de un tejado frágil, por un foso de ascensor sin protección, por el hueco de una escalera).|Primero, ¿qué es un izaje? Podemos entender lo que es un izaje como una forma de levantar o mover objetos con ayuda de algunos dispositivos, el cual se hace de una forma segura, controlada y bien calculada.|Un espacio confinado o recinto confinado es aquel que dispone de aberturas de entrada reducidas, una ventilación natural desfavorable y no está concebido para permanecer en su interior. Por ello, puede presentar una atmósfera irrespirable y albergar gases, vapores o partículas tóxicas o inflamables.|Se considera riesgo eléctrico cuándo existe una posibilidad de contacto del cuerpo humano con la corriente eléctrica y que puede resultar un peligro para la integridad de las personas.|De forma más concreta se podría definir un extintor como un aparato autónomo, diseñado como un cilindro, que puede ser desplazado por una sola persona y que usando un mecanismo de impulsión bajo presión de un gas o presión mecánica, lanza un agente extintor hacia la base del fuego, para lograr extinguirlo.";
 			}
 			else if (lang == "italiano")
@@ -141,7 +141,7 @@ public class AccountController : Controller
 				textopp3 = "PPROTECC nasce come strumento gestionale per la prevenzione degli infortuni sul lavoro in quota, per rafforzare l'applicazione";
 				textopp4 = "delle politiche organizzative e garantire il tempestivo rispetto della normativa e dei regolamenti vigenti.";
 				modulos = "Lavori in quota|Sollevamento carichi|Spazi confinati|Rischio elettrico|Gestione estintori";
-				subtitulos = "Il modo migliore per gestire un rischio è conoscerlo|Il sollevamento di carichi è il processo più importante?|PROSSIMAMENTE|PROSSIMAMENTE|PROSSIMAMENTE";
+				subtitulos = "Il modo migliore per gestire un rischio è conoscerlo|Il sollevamento di carichi è il processo più importante?|Prossimamente|Prossimamente|Prossimamente";
 				descripciones = "I lavori in quota sono lavori che vengono eseguiti in qualsiasi luogo in cui, se non sono state prese le precauzioni necessarie, una persona può cadere da un'altezza che può causare lesioni (caduta da un tetto fragile, da un vano ascensore non protetto giù da una tromba delle scale ).|Innanzitutto, cos'è un ascensore? Possiamo capire cos'è un paranco come mezzo per sollevare o spostare oggetti con l'ausilio di alcuni dispositivi, cosa che avviene in modo sicuro, controllato e ben calcolato.|Uno spazio confinato o spazio confinato è uno che ha aperture di ingresso ridotte, ventilazione naturale sfavorevole e non è progettato per rimanere all'interno. Pertanto, può presentare un'atmosfera irrespirabile e ospitare gas, vapori o particelle tossici o infiammabili.|Il rischio elettrico è considerato quando esiste la possibilità di contatto del corpo umano con la corrente elettrica e ciò può rappresentare un pericolo per l'integrità delle persone.|Più precisamente, un estintore potrebbe essere definito come un dispositivo autonomo, concepito come una bombola, che può essere spostata da una sola persona e che, tramite un meccanismo di azionamento a pressione di gas o meccanica, lancia un agente estinguente verso la base. il fuoco, per estinguerlo.";
 				piedepaginacards = "Ultime ispezioni|6 mesi fa";
 			}
